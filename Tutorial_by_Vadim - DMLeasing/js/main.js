b605230cf87c839b4a4abe7653cc325adefeb7fa
Roman__Mal'cev <61384075+DevFromSiberia@users.noreply.github.com>
2023-09-18 22:56:25 +0700

    DMLeasing: Added project without adaptive

---
+++ Tutorial_by_Vadim - DMLeasing/js/main.js
@@ -0,0 +1,34 @@
+const tabItem = document.querySelectorAll(".tabs__btn-item");
+const tabContent = document.querySelectorAll(".tabs__content-items");
+
+tabItem.forEach(function (element) {
+  element.addEventListener("click", open);
+});
+
+function open(e) {
+  const tabTarget = e.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabItem.forEach((item) => {
+    item.classList.remove("tabs__btn-item--active");
+  });
+  tabTarget.classList.add("tabs__btn-item--active");
+
+  tabContent.forEach((item) => {
+    item.classList.remove("tabs__content-items--active");
+  });
+  document
+    .querySelector(`#${button}`)
+    .classList.add("tabs__content-items--active");
+}
+
+const swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  effect: "fade",
+});
