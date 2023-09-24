f3aa8e5be02d590915243403da827a47d025625f
Roman__Mal'cev <61384075+DevFromSiberia@users.noreply.github.com>
2023-09-24 19:16:54 +0700

    Desire: Added rightside-menu

--- Tutorial_by_Vadim-Desire/app/js/main.js
+++ Tutorial_by_Vadim-Desire/app/js/main.js
@@ -1,3 +1,13 @@
 $(function () {
 
-})
+  $(".header__btn").on("click", function () {
+    $(".rightside-menu").removeClass("rightside-menu--close");
+    console.log('remove')
+  });
+
+  $(".rightside-menu__close").on("click", function () {
+    $(".rightside-menu").addClass("rightside-menu--close");
+    console.log("adsd");
+  });
+
+});
